40d44c4dc210806e7ed3a87e02cd7a079f7c677f
JoshuaStudebaker <j.s.studebaker1@gmail.com>
2020-09-18 22:41:16 -0600

    abstracted form component

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -2,6 +2,29 @@
   <div class="home">
     <h1>Welcome</h1>
     <!-- TODO Create Form with newBlog inputs -->
+    <div>
+      <form class="form-inline" @submit.prevent="createBlog">
+        <div class="form-group">
+          <input
+            type="text"
+            v-model="newBlog.title"
+            class="form-control"
+            placeholder="Blog Title"
+            aria-describedby="helpId"
+          />
+          <input
+            type="text"
+            class="form-control"
+            v-model="newBlog.body"
+            placeholder="Type..."
+            aria-describedby="helpId"
+          />
+        </div>
+        <button type="submit" class="btn btn-success">
+          <i class="fa fa-plus" aria-hidden="true"></i>
+        </button>
+      </form>
+    </div>
     <blogs-component v-for="iBlog in blogs" :key="iBlog.id" :blogProp="iBlog" />
   </div>
 </template>
